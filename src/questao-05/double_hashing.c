50341e6d4f97e3bcb41f913ed09e8cabd0cdfdce
Leandro Bentes <lm25ttd@gmail.com>
2013-04-25 01:38:49 -0400

    [src-questao-5] Implementada leitura do arquivo de entrada 		Melhoria do método de busca

--- src/questao-05/double_hashing.c
+++ src/questao-05/double_hashing.c
@@ -3,94 +3,120 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_VALOR_ENTRADA 100
-
 #define VALOR_NULO -1
-#define TAM_HASH 10
-#define FUNCAO_HASHING (valor_entrada % TAM_HASH)
+#define TAM_TABELA 100000 //Tamanho da tabela especificado 10^5
 
-#define FUNCAO_DOUBLE_HASHING ((valor_entrada % 2)*2 +1)
+#define FUNCAO_HASHING (valorEntrada % TAM_TABELA)
+#define FUNCAO_DOUBLE_HASHING ((valorEntrada % 2)*2 +1)
 
-int tabela_hash[TAM_HASH];
-int total_valores_inseridos=0;
+struct elementoTabela
+{
+    int indice;
+    int valor;
+} elementoTabela;
 
-int gerar_hashing ( int valor_entrada )
+int tabelaHash[TAM_TABELA];
+int quantidadeValoresInseridos=0;
+struct elementoTabela valorEncontrado = {VALOR_NULO, VALOR_NULO};
+
+
+int gerar_hashing (int valorEntrada)
 {
     return FUNCAO_HASHING;
 }
 
-int gerar_double_hashing ( int valor_entrada )
+int gerar_double_hashing (int valorEntrada)
 {
     return FUNCAO_DOUBLE_HASHING;
 }
 
-void inserir_valor_tabela ( int valor_entrada )
+void inserir_valor_tabela (int valorEntrada)
 {
-    int indice = gerar_hashing ( valor_entrada );
-    int salto = gerar_double_hashing ( valor_entrada );
+    int indice = gerar_hashing (valorEntrada);
+    int salto = gerar_double_hashing (valorEntrada);
 
-    while ( tabela_hash[indice]!=-1 )
+    while (tabelaHash[indice]!=VALOR_NULO)
     {
-        indice = ( indice + salto ) % TAM_HASH ;
-        printf ( "[%d]Salto para %d\n",valor_entrada, indice ); //FIXME Apenas para uso em Debug
-        salto=gerar_double_hashing ( indice );
-    }
-    tabela_hash[indice] = valor_entrada;
-    total_valores_inseridos++;
-}
-
-int buscar_valor ( int valor_entrada )
-{
-    int indice = gerar_hashing ( valor_entrada );
-    int salto = gerar_double_hashing ( valor_entrada );
-    int tentativas_de_busca=0;
-    while ( tabela_hash[indice]!=VALOR_NULO )
-    {
-        if ( tabela_hash[indice]==valor_entrada )
-            return tabela_hash[indice];
-	if (tentativas_de_busca>=total_valores_inseridos)
-	    break;
-        tentativas_de_busca++;
-        indice = ( ( indice + salto ) % TAM_HASH );
+        indice = (indice + salto) % TAM_TABELA ;
     }
 
-    return VALOR_NULO;
+    tabelaHash[indice] = valorEntrada;
+    quantidadeValoresInseridos++;
 }
 
-int gerar_numero_randomico()
+struct elementoTabela buscar_valor (int valorEntrada)
 {
-    return rand() % MAX_VALOR_ENTRADA;
+    int indice = gerar_hashing (valorEntrada);
+    int salto = gerar_double_hashing (valorEntrada);
+    int tentativasDeBusca=0;
+    while (tabelaHash[indice]!=VALOR_NULO)
+    {
+        if (tabelaHash[indice]==valorEntrada)
+        {
+            valorEncontrado.indice=indice;
+            valorEncontrado.valor= tabelaHash[indice];
+        }
+        if (tentativasDeBusca>=quantidadeValoresInseridos)
+            break;
+        tentativasDeBusca++;
+        indice = (indice + salto) % TAM_TABELA;
+    }
+
+    return valorEncontrado;
 }
 
 void inicializar_tabela()
 {
     int i=0;
-    for ( i=0; i< TAM_HASH; i++ )
+    for (i=0; i< TAM_TABELA; i++)
     {
-        tabela_hash[i] = VALOR_NULO;
+        tabelaHash[i] = VALOR_NULO;
     }
 }
 
-int main ( int argc, char *argv[] )
+void carregar_dados_na_tabela(const char *nomeArquivo)
 {
-    srand ( time ( NULL ) );
-    inicializar_tabela();
-    printf ( "Ambiente inicializado!\n" );
+    freopen(nomeArquivo, "r", stdin);
 
-    int i=0;
-
-    for ( i=0; i< TAM_HASH; i++ )
+    int numeroAtual=0;
+    while (scanf("%d ", &numeroAtual) != EOF)
     {
-        inserir_valor_tabela ( gerar_numero_randomico() );
+        inserir_valor_tabela(numeroAtual);
     }
 
-    for ( i=0; i< TAM_HASH; i++ )
+    fclose(stdin);
+}
+
+int main (int argc, char *argv[])
+{
+
+    if (argc<3)
     {
-        printf ( "Valor da tabela[%d]: %d \n", i, tabela_hash[i] );
+        printf ( "Parâmetros inválidos!\nUtilize: ./double_hashing <arquivo_com_dados> <numero_para_busca>\n" );
+        return 1;
     }
+    else
+    {
+        srand (time(NULL));
+        inicializar_tabela();
+        printf ("Ambiente inicializado...\n");
 
-    int elemento_buscado=gerar_numero_randomico();	
-    printf("Elemento[%d] %d\n", elemento_buscado, buscar_valor(elemento_buscado));
+        const char *nomeArquivo = (const char*)argv[1];
+        int valorBuscado = atoi((const char*)argv[2]);
 
-    return 0;
+        carregar_dados_na_tabela(nomeArquivo);
+        printf ("Dados carregados...\n");
+
+        struct elementoTabela retornoBusca = buscar_valor(valorBuscado);
+
+        if (retornoBusca.valor!=VALOR_NULO)
+        {
+            printf("Valor [%d] encontrado no índice %d\n", retornoBusca.valor, retornoBusca.indice);
+        }
+        else
+        {
+            printf("O valor desejado não existe na tabela.\n");
+        }
+        return 0;
+    }
 }
