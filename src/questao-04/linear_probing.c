2e52eb2541970c918b3d10425909b37bbbd23743
iasmim cunha <iasmim.souza26@gmail.com>
2013-04-27 04:00:38 -0400

    [src-questao-4] Adicionada ultima versao

--- src/questao-04/linear_probing.c
+++ src/questao-04/linear_probing.c
@@ -15,34 +15,65 @@ struct campo
  int valor;
 }campo;
 
-/*metódo de busca na tabela hash*/
-struct campo busca(int v)
+
+void inicializar_tabela()
+{
+    int i=0;
+    for (i=0; i< M; i++)
+    {
+        tab[i] = -1;
+    }
+}
+
+
+/*
+ * Função:busca_tabela
+ *
+ * metódo de busca na tabela hash
+ * compara todos os valores da tabela M vezes,onde M = qtd de itens na tabela,
+ * com o valor da busca,caso este exista retorna
+ * assim que encontrar ,caso não retorna valor nulo -1
+ *
+ * */
+struct campo busca_tabela(int v)
 {
 	int b;
 	struct campo campoVazio;
 
+	puts("\nBuscando...\n");
 	campoVazio.indice = -1;
 	campoVazio.valor = -1;
 
+	int tentativas_busca = 0;
 	struct campo campoOcupado;
-		b = hash(v);
-		while (tab[b] != 0)
+		b = hash(v);//gera o indice do valor
+		while (tentativas_busca < M)
 		{
 			 if (tab[b] == v)
-			  {
-			   campoOcupado.indice = b;
-			   campoOcupado.valor = tab[b];
-			   return  campoOcupado;
-			  }
+			 {
+			  campoOcupado.indice = b;
+			  campoOcupado.valor = tab[b];
+
+			  return  campoOcupado;
+			 }
 			else
-			  {
-			   b = (b + 1) % M;
-			  }
+			 {
+			  b = (b + 1) % M;
+			 }
+
+			 tentativas_busca++;
 		}
+     printf("\nTentativas de busca:%d\n",tentativas_busca);
 	 return  campoVazio;
 }
 
-int *ler(const char *arquivo)
+/*
+ * Função:ler_arquivo
+ * Ler o arquivo ,cujo caminho vem por parametro no argv
+ * e coloca em um vetor de interos intermediario
+ *
+ * */
+int *ler_arquivo(const char *arquivo)
 {
 	    FILE *arq;
 	    int j;
@@ -56,11 +87,11 @@ int *ler(const char *arquivo)
 		    while(fgets(linha, sizeof linha, arq) != NULL)
 		    {
 		        int len = strlen(linha) - 1;
-		        if (linha[len] == '\n') /*Verifica se o ultimo caracter é um enter*/
-		                linha[len] = 0; /*Isso substitui o enter por um '\0', que indica o fim da string*/
+		        if (linha[len] == '\n')
+		                linha[len] = 0;
 		        if(strlen(linha) > 0)
 		        {
-		        	valores_entrada[j] = atoi(&linha); /*Converte um char para int*/
+		        	valores_entrada[j] = atoi(&linha);
 		            j++;
 		        }
 		    }
@@ -83,19 +114,23 @@ int main(int argc, char *argv[]) {
 	tab = malloc(M * sizeof (NUMERO_MAX));
 	valores_aleatorios = malloc(M * sizeof (NUMERO_MAX));
 	//Pega os valores aleatorios gerados no script
-	valores_aleatorios = ler((const char*)argv[1]);
+	valores_aleatorios = ler_arquivo((const char*)argv[1]);
 
+	inicializar_tabela();
+	int inseridos = 0;
 	puts("Iniciando...\n\n");
 	    for (j=0; j< M; j++)
 	     {
 			  c = hash(valores_aleatorios[j]);
-			     while (tab[c] != 0)
+			     while (tab[c] != -1)
 			        c = (c + 1) % M;
 			     tab[c] = valores_aleatorios[j];
-	     }
-    puts("Tabela Hash preenchida!");
 
-    resultado =  busca(atoi((const char*)argv[2]));
+			     inseridos++;
+	     }
+    printf("Tabela Hash preenchida!\nquantidade de elementos inseridos:%d\n\n",inseridos);
+
+    resultado =  busca_tabela(atoi((const char*)argv[2]));
 
     if(resultado.indice != -1)
       printf("\nValor da tabela[%d]:%d\n",resultado.indice,resultado.valor);
@@ -103,6 +138,5 @@ int main(int argc, char *argv[]) {
       puts("Valor não encontrado\n");
 	return 0;
 }
-
 
 
